95a29ebea345b709733bc9f8be395f4b998e7092
DwiSaputra01 <98682012+DwiSaputra01@users.noreply.github.com>
2023-11-13 21:24:08 +0700

    Create download_models.py

---
+++ download_models.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import requests
+
+MDX_DOWNLOAD_LINK = 'https://github.com/TRvlvr/model_repo/releases/download/all_public_uvr_models/'
+
+mdxnet_models_dir = 'models'
+
+def dl_model(link, model_name, dir_name):
+    with requests.get(f'{link}{model_name}') as r:
+        r.raise_for_status()
+        with open(dir_name / model_name, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+
+if __name__ == '__main__':
+    mdx_model_names = ['UVR-MDX-NET-Voc_FT.onnx', 'UVR_MDXNET_KARA_2.onnx', 'Reverb_HQ_By_FoxJoy.onnx', 'Kim_Vocal_2.onnx']
+    for model in mdx_model_names:
+        print(f'Downloading {model}...')
+        dl_model(MDX_DOWNLOAD_LINK, model, mdxnet_models_dir)
+
+    print('All models downloaded!')
